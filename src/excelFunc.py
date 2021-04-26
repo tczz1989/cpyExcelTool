6fe44dc98504362d50f575999ab0bc53bc255af3
Tiansheng Wang <wangts@hitrobotgroup.com>
2021-04-26 22:50:49 +0800

    add function for .xlsx format excel

--- src/excelFunc.py
+++ src/excelFunc.py
@@ -1,5 +1,7 @@
 import xlrd
 import xlwt
+from openpyxl import load_workbook
+# from openpyxl import Workbook
 # from datetime import date, datetime
 
 
@@ -69,13 +71,13 @@ def read_excel(sheet, rows, cols):
     #
     # # 获取单元格内容的数据类型
     # print(sheet.cell(1, 0).ctype)
-    print(data)
+    # print(data)
     return data
 
 
-def write_excel(output_filename, input_data, output_rows, output_cols, *sheet):
+def write_excel_xls(output_filename, input_data, output_rows, output_cols, *sheet):
     # # 创建一个workbook 设置编码
-    workbook = xlwt.Workbook(encoding='utf-8')
+    workbook = xlwt.Workbook(encoding='ascii')
     if sheet is None or sheet is () or sheet is [] or sheet is "":
         # 创建一个worksheet
         sheet = workbook.add_sheet('Sheet1')
@@ -113,6 +115,48 @@ def write_excel(output_filename, input_data, output_rows, output_cols, *sheet):
     return
 
 
+def write_excel_xlsx(output_filename, input_data, output_rows, output_cols, sheet_name):
+    # Create a workbook and add a worksheet.
+    workbook = load_workbook("../output/" + output_filename)
+    # worksheet = workbook.add_worksheet()
+    if sheet_name in workbook.get_sheet_names():
+        sheet = workbook.get_sheet_by_name(sheet_name)
+    else:
+        sheet = workbook.create_sheet(sheet_name)
+    # 写入excel
+    try:
+        index = 0
+        for i, row in enumerate(output_rows):
+            for col in output_cols:
+                if len(row) is 1 and len(col) is 1:
+                    # 参数对应 行, 列, 值
+                    sheet.cell(row+1, col+1, label=input_data[index])
+                    index += 1
+                    # 保存
+                elif len(row) is 2 and len(col) is 2:
+                    for r in range(row[0], row[1]+1):
+                        for c in range(col[0], col[1]+1):
+                            sheet.cell(r+1, c+1, input_data[index])
+                            index += 1
+                elif len(row) is 2 and len(col) is 1:
+                    for r in range(row[0], row[1]+1):
+                        sheet.cell(r+1, col[0]+1, input_data[index])
+                        index += 1
+                elif len(row) is 1 and len(col) is 2:
+                    for c in range(col[0], col[1]+1):
+                        sheet.cell(row[0]+1, c+1, input_data[index])
+                        index += 1
+                else:
+                    print("row col input error!")
+                    return
+            workbook.save('../output/'+output_filename)
+    # except PermissionError:
+    #     print("请关闭写入的目标文件！")
+    except Exception as e:
+        print(str(e))
+    return
+
+
 def get_col_name(input_str):
     sections = input_str.split(",")
     col_ranges = []
@@ -133,7 +177,7 @@ def get_row_name(input_str):
         row_range_str = section.split("-")
         row_range_num = []
         for row in row_range_str:
-            row_range_num.append(int(row.strip()))
+            row_range_num.append(int(row.strip())-1)
         row_ranges.append(row_range_num)
     print(row_ranges)
     return row_ranges
@@ -155,3 +199,21 @@ def input_output_match(input_rows, input_cols, output_rows, output_cols):
             if (col[1] - col[0]) != (output_cols[i][1] - output_cols[i][0]):
                 return False
     return True
+
+
+def cpy_excel_main(input_config, output_config):
+    workbook = open_excel(input_config[0][1])
+    sheet = open_sheet(workbook)
+    # e.read_excel(filename)
+    rows = get_row_name(input_config[2][1])
+    cols = get_col_name(input_config[3][1])
+    rows1 = get_row_name(output_config[2][1])
+    cols1 = get_col_name(output_config[3][1])
+    print(rows, cols, rows1, cols1)
+    if input_output_match(rows, cols, rows1, cols1):
+        print("config is correct")
+        data = read_excel(sheet, rows, cols)
+        write_excel_xlsx(output_config[0][1], data, rows, cols, output_config[1][1])
+    else:
+        print("input output config not match")
+
